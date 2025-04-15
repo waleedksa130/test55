b97237dc42c302f19dbb455812c65d2ede8b0190
AlWaleed <WALEEDKSA130@GMAIL.COM>
2025-04-15 10:18:18 +0300

    Update homepage.dart

--- homepage.dart
+++ homepage.dart
@@ -1,79 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:test122/applocal/applocal.dart';
+import 'nextpage.dart';
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:project/applocal/applocal.dart';
-import 'package:project/nextpage.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-class HomePage extends StatefulWidget{
   @override
-  State<StatefulWidget> createState()=>_HomePage();
-  final namecntroller= TextEditingController();
-  
+  State<HomePage> createState() => _HomePageState();
 }
-class _HomePage extends State<HomePage>{
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("${getLang(context, "tital")}"),
+        title: Text("${getLang(context, "home_page")}"),
+        centerTitle: true,
       ),
-      body:Container( margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        child:  Column(
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
           children: [
 
+
+            const SizedBox(height: 30),
             TextField(
-              controller: widget.namecntroller,
+              controller: nameController,
               decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: "${getLang(context, "name")}",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)))),
-            ),
-            SizedBox(height: 100,),
-            Container(
-              margin: EdgeInsets.fromLTRB(30, 60, 30, 0),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(2)),
-                  border: Border.all(color: Colors.black, width: 2)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>NextPage(name: widget.namecntroller.text)));
-                      },
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: Size(140, 70),
-                          shape: BeveledRectangleBorder(
-                              borderRadius: BorderRadius.circular(3),
-                              side: BorderSide(color: Colors.black))),
-                      child: Text("${getLang(context, "next")}",
-                          style: TextStyle(fontSize: 25, color: Colors.black))),
-                  SizedBox(width: 10,),
-                  ElevatedButton(
-                      onPressed: () {
-                        exit(0);
-                      },
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: Size(140, 70),
-                          shape: BeveledRectangleBorder(
-                              borderRadius: BorderRadius.circular(3),
-                              side: BorderSide(color: Colors.black))),
-                      child: Text("${getLang(context, "close")}",
-                          style: TextStyle(fontSize: 25, color: Colors.black)))
-                ],
+                hintText: getLang(context, "your_name"),
+                border: OutlineInputBorder(),
               ),
             ),
-
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (nameController.text.trim().isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              NextPage(name: nameController.text),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text(getLang(context, "next")),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    exit(0); // للخروج من التطبيق
+                  },
+                  child: Text(getLang(context, "close")),
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
-  
 }
